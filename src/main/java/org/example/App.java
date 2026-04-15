c390085787e6dec3a91ec39f28653b091416e58e
pooja1231 <pujashegokar26@gmail.com>
2026-04-16 00:04:08 +0530

    Added project directory

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class App {
     public static void main(String[] args) {
-
+        System.out.println("Pooja");
         System.out.println("Hello DevOps World!");
     }
 }
